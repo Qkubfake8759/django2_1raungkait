0dca5e930be4c849a5dc4a3999fc800531f2186b
Qkubfake8759 <6629011009@cdti.ac.th>
2024-08-28 16:26:05 +0700

    first commit

---
+++ myweb/contact/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def contact(request):
+  template = loader.get_template('contact.html')
+  return HttpResponse(template.render())
+
